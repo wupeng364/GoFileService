6b280c0bf0becc08591bc5ddca66f06b260d7f0d
wupeng364 <wupeng364@outlook.com>
2019-05-24 16:10:15 +0800

    清除冗余代码

--- bin/static/lib/import-core.js
+++ bin/static/lib/import-core.js
@@ -1,21 +1,20 @@
-
 if(typeof __import_core__=="undefined"){   
 	__import_core__=true;  
-	var version = "2019.5.19"; 
+	var version = "2019.5.24"; 
 	// JS
-	document.write("<script type='text/javascript' src='/lib/axios.min.js?v="+version+"'></script>");
-	document.write("<script type='text/javascript' src='/lib/promise.auto.min.js?v="+version+"'></script>");
-	document.write("<script type='text/javascript' src='/lib/utils/prop-extends.js?v="+version+"'></script>");
+	document.write("<script type='text/javascript' src='/lib/common/extends.js?v="+version+"'></script>");
 	document.write("<script type='text/javascript' src='/lib/common/tools.js?v="+version+"'></script>");
+	document.write("<script type='text/javascript' src='/lib/3party_library/axios.min.js?v="+version+"'></script>");
+	document.write("<script type='text/javascript' src='/lib/3party_library/promise.auto.min.js?v="+version+"'></script>");
+	document.write("<script type='text/javascript' src='/lib/3party_library/vue/vue.min.js?v="+version+"'></script>");
+	document.write("<script type='text/javascript' src='/lib/3party_library/vue/vue-router.js?v="+version+"'></script>");
+	document.write("<script type='text/javascript' src='/lib/3party_library/iview/iview.min.js?v="+version+"'></script>");
 	document.write("<script type='text/javascript' src='/lib/utils/futil.js?v="+version+"'></script>");
 	document.write("<script type='text/javascript' src='/lib/utils/fhttp.js?v="+version+"'></script>");
-	document.write("<script type='text/javascript' src='/lib/vue/vue.min.js?v="+version+"'></script>");
-	document.write("<script type='text/javascript' src='/lib/vue/vue-router.js?v="+version+"'></script>");
-	document.write("<script type='text/javascript' src='/lib/iview/iview.min.js?v="+version+"'></script>");
 	document.write("<script type='text/javascript' src='/lib/vue.components.js?v="+version+"'></script>");
 	
 	// CSS
-	document.write("<link rel='stylesheet' type='text/css' href='/lib/iview/styles/iview.css?v="+version+"'/>");
+	document.write("<link rel='stylesheet' type='text/css' href='/lib/3party_library/iview/styles/iview.css?v="+version+"'/>");
 	document.write("<link rel='stylesheet' type='text/css' href='/css/common.css?v="+version+"'/>");
 
 	{ // 停止所有拖拽动作
