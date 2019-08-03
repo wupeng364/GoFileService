1b512b349e2a60ff92999c4d0f1d615e8ebfe7ec
伍朋 <wupeng364@outlook.com>
2019-08-04 02:10:45 +0800

    清除

--- src/gofs/modules/common/sqlite/constant.go
+++
@@ -1,7 +0,0 @@
-package sqlite
-import(
-	// "errors"
-)
-const(
-	cfg_db_path = "./config/gofs.db"
-)
--- src/gofs/modules/extend/htmlpage/constant.go
+++
@@ -1,6 +0,0 @@
-package htmlpage
-
-const(
-	cfg_http_static	= "http.static"
-	StaticSource_BasePath = "/"
-)
